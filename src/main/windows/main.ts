c23828d1687e26d63bfd215cda3b46f98c00a107
Zerubbabel <zerubt16@gmail.com>
2026-05-10 16:59:18 +0300

    Initialize project with Electron app boilerplate

---
+++ src/main/windows/main.ts
@@ -0,0 +1,39 @@
+import { BrowserWindow } from 'electron'
+import { join } from 'node:path'
+
+import { createWindow } from 'lib/electron-app/factories/windows/create'
+import { displayName } from '~/package.json'
+
+export async function MainWindow() {
+  const window = createWindow({
+    id: 'main',
+    title: displayName,
+    width: 700,
+    height: 473,
+    show: false,
+    center: true,
+    movable: true,
+    resizable: true,
+    alwaysOnTop: false,
+    autoHideMenuBar: true,
+
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false,
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  window.webContents.on('did-finish-load', () => {
+    window.show()
+  })
+
+  window.on('close', () => {
+    for (const window of BrowserWindow.getAllWindows()) {
+      window.destroy()
+    }
+  })
+
+  return window
+}
